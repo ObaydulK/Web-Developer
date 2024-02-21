8182e8e25221210c8660b6e9f573c14dedc5e798
obaydul_Kuasha <obaydul00000@gmail.com>
2024-02-21 21:21:58 +0600

    add about and education align ment

--- src/Component/Pages/About/AboutEducation/AboutEducation.jsx
+++ src/Component/Pages/About/AboutEducation/AboutEducation.jsx
@@ -10,24 +10,24 @@ const AboutEducation = () => {
                     <p className=" px-3 text-justify ">Hello, I'm Obaydul_Kuasha, a Computer Science Engineering (BSE) student currently in the program. My academic journey involves delving into algorithms, data structures, and various programming languages. I'm passionate about software engineering, engaging in hands-on projects that enhance my problem-solving abilities. As a part of my curriculum, I'm immersed in diverse subjects like machine learning  and databases . This comprehensive education equips me with the skills needed to navigate the ever-evolving field of computer science. I am dedicated to staying current with industry trends, ensuring that I contribute effectively to the advancements in technology and innovation.</p>
                 </div>
 
-                <div className="grid grid-flow-row lg:grid-cols-2 items-center align-middle sm:grid px-4 ">
-                    <div className=" " data-aos="fade-right" data-aos-duration="500">
-                        <img src="https://themebeyond.com/html/kufa/img/banner/banner_img2.png" className="max-w-sm  " />
+                <div className="grid grid-flow-row lg:grid-cols-2 sm:grid p-4 " style={{ justifyItems: 'center' }} >
+                    <div className=" " >
+                        <img src="https://themebeyond.com/html/kufa/img/banner/banner_img2.png" className="max-w-sm  " data-aos="fade-right" data-aos-duration="500" />
                     </div>
-                    <div className="    ">
+                    <div className=" ">
                         <h1 className="text-5xl font-bold">Education</h1>
-                        <div className="   me-auto  ">
-                            <div className=" flex flex-row gap-5 items-center   py-5" data-aos="zoom-out-right" data-aos-duration="2000">
+                        <div>
+                            <div className=" flex flex-row gap-5 items-center    py-5" data-aos="zoom-out-right" data-aos-duration="2000">
                                 {/* <h1 className="text-emerald-400 text-2xl font-bold">2023 to 2027</h1> */}
                                 <p>------</p>
                                 <div className="font-bold">
                                     <p className="text-2xl">BSc in computer Science</p>
                                     <p className=""> World University of Bangladeshi, Uttara</p>
                                     <p className="text-2xl">0.00 Out of 4</p>
-                                    <progress className="progress w-full" value="20" max="100"></progress>
+                                    <progress className="progress w-full" value="40" max="100"></progress>
                                 </div>
                             </div>
-                            <div className=" flex flex-row gap-5 items-center  py-5" data-aos="zoom-out-left" data-aos-duration="2000">
+                            <div className=" flex flex-row gap-5 items-center   py-5" data-aos="zoom-out-left" data-aos-duration="2000">
                                 {/* <h1 className="text-emerald-400 text-2xl font-bold">2018 to 2022</h1> */}
                                 <p>------</p>
                                 <div className="font-bold">
@@ -37,7 +37,7 @@ const AboutEducation = () => {
                                     <progress className="progress w-full" value="100" max="100"></progress>
                                 </div>
                             </div>
-                            <div className=" flex flex-row gap-5 items-center  py-5" data-aos="zoom-out-right" data-aos-duration="2000">
+                            <div className=" flex flex-row gap-5 items-center   py-5" data-aos="zoom-out-right" data-aos-duration="2000">
                                 {/* <h1 className="text-emerald-400 text-2xl font-bold">2016 to 2018</h1> */}
                                 <p>------</p>
                                 <div className="font-bold">
@@ -47,7 +47,6 @@ const AboutEducation = () => {
                                     <progress className="progress w-full" value="100" max="100"></progress>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
